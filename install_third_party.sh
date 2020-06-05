4a0ec7369ee887b2165ff916d36474a50495ab69
MattShilling <shillinm@oregonstate.edu>
2020-06-04 23:02:45 -0700

    At this point this file is really just 'paste bash commands I used'

--- install_third_party.sh
+++ install_third_party.sh
@@ -2,11 +2,12 @@
 
 mkdir -p third_party/ 
 cd third_party
-git clone https://github.com/libglui/glui.git 2>/dev/null 
+git clone https://github.com/libglui/glui.git 2>/dev/null
 mkdir -p build
 cd glui
-cmake -DCMAKE_INSTALL_PREFIX:PATH=third_party/build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=~/code/cs475/ParticleSystem/third_party/build
 make install -j4
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:third_party/build/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/code/cs475/ParticleSystem/third_party/build/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/apps/cuda/cuda-10.1/lib64/
 export LD_LIBRARY_PATH
