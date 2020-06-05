72fce31252e1d631741a35f01deee9a1a3d2e1f1
matts2 <matts@reliable.co>
2020-06-04 17:07:16 -0700

    Updating install script.

--- install_third_party.sh
+++ install_third_party.sh
@@ -5,7 +5,7 @@ cd third_party
 git clone https://github.com/libglui/glui.git 2>/dev/null 
 mkdir -p build
 cd glui
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=third_party/build
 make install -j4
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:third_party/build/lib
